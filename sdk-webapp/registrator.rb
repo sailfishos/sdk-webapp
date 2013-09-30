63460759119c41124d3a49b1fc27a2fb84e16256
Jarko Vihriala <jarko.vihriala@jollamobile.com>
2013-09-30 21:02:57 +0300

    [sdk-register] make registration view optional

--- sdk-webapp/registrator.rb
+++ sdk-webapp/registrator.rb
@@ -1,6 +1,28 @@
 require 'json'
 require './process.rb'
 
+def registration_visibility
+
+  domain="foo",raw_domain="",
+  regdomain="", raw_regdomain=""
+
+  raw_domain=`sdk-register -d`
+  unless raw_domain == nil
+    domain=raw_domain[0,raw_domain.length-1]
+  end
+  
+  raw_regdomain=File.open( "/etc/ssu/reg_domain" ).first if File::exists?( "/etc/ssu/reg_domain" )
+  unless raw_regdomain == nil
+    regdomain=raw_regdomain[0,raw_domain.length-1]
+  end
+
+  if domain==regdomain
+    return "normal"
+  else
+    return "none"
+  end
+end
+
 class Registrator
 
   def id
